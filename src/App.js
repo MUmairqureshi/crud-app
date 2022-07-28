9c99a6d7a7b4a6ea36779f43a08721f1369a16ca
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2022-07-27 22:51:32 -1200

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './taskform/form.css';
+import TodoList from "./crudopt2/TodoList"
+import { Todoapp } from "./crudopt/todoapp"
+import {TaskProvider} from './Form/hook/context'
+import {Container} from './Form/container'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<TaskProvider>   
+    <div>
+<Container/>
     </div>
+    </TaskProvider>
   );
 }
 
 export default App;
+
